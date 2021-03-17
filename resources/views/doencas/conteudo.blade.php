a635d2a5462c2faafdbe3558a9f044f337e0d88f
Victor Andrade <victor.hugooo@hotmail.com.br>
2021-03-17 13:56:56 -0300

    Adicionado tratamento para post de multiplos vetores

--- resources/views/doencas/conteudo.blade.php
+++ resources/views/doencas/conteudo.blade.php
@@ -1,11 +1,65 @@
 @extends('template.template')
 
 @section('conteudo')
+    <style>
+        /* Remove margins and padding from the list */
+        ul {
+            margin: 0;
+            padding: 0;
+        }
+
+        /* Style the list items */
+        ul li {
+            display: flex;
+            flex-direction: row;
+            flex-grow: 1;
+            align-items: space-between;
+            cursor: pointer;
+            position: relative;
+            padding: 12px 8px 12px 8px;
+            list-style-type: none;
+            background: #eee;
+            font-size: 18px;
+            transition: 0.2s;
+
+            /* make the list items unselectable */
+            -webkit-user-select: none;
+            -moz-user-select: none;
+            -ms-user-select: none;
+            user-select: none;
+        }
+
+        ul li input{
+            flex: 10;
+        }
+        /* Set all odd list items to a different color (zebra-stripes) */
+        ul li:nth-child(odd) {
+            background: #f9f9f9;
+        }
+
+        /* Darker background-color on hover */
+        ul li:hover {
+            background: #ddd;
+        }
+
+        /* Style the close button */
+        .close{
+            flex:1;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+        }
+
+        .close:hover {
+            background-color: #f44336;
+            color: white;
+        }
+    </style>
 <div class="jumbotron text-center">
     <h1>Cadastrar Nova Doença</h1>
 </div>
 
-   <div class="container">
+<div class="container">
     @if(isset($errors) && count($errors)>0)
         <div class="text-center alert-danger">
             @foreach($errors->all() as $erro)
@@ -15,11 +69,11 @@
     @endif
 
     @if(isset($doenca))
-    <form action name="edit" id="create" method="post" action="{{url('doencas.update')}}">
-    @method('PUT')
-@else
-    <form action name="create" id="create" method="post" action="{{url('doencas.store')}}">
-        @endif
+        <form action name="edit" id="create" method="post" action="{{url('doencas.update')}}">
+        @method('PUT')
+    @else
+        <form action name="create" id="create" method="post" action="{{url('doencas.store')}}">
+    @endif
         @csrf
         <div class="form-group">
             <label class="form-control-label col-sm-2" for="nome">Nome:</label>
@@ -56,8 +110,13 @@
         <div class="form-group t-10">
             <label class="control-label col-sm-2" for="vector">Vetor:</label>
             <div class="col-sm-10">
-                <input type="text" class="form-control" id="vector" name="vector"
-                       placeholder="Digite um vetor" value="{{$doenca['vector'][0] ?? ''}}" required>
+                <input class="form-control" type="text" id="myInput" placeholder="Digite o vetor">
+                <span onclick="newElement()" class="btn btn-success btn-group-lg">Add</span>
+            </div>
+
+            <div class="col-sm-5">
+                <ul id="myUL" >
+                </ul>
             </div>
         </div>
 
@@ -215,6 +274,58 @@
         for(let i = 0; i< estados.length; i++){
             if(estadosRecebidos.includes(estados[i].value)){
                 estados[i].checked = true;
+            }
+        }
+    }
+</script>
+<script>
+    // Create a "close" button and append it to each list item
+    var myNodelist = document.getElementsByTagName("LI");
+    var i;
+    for (i = 0; i < myNodelist.length; i++) {
+        var span = document.createElement("SPAN");
+        var txt = document.createTextNode("\u00D7");
+        span.className = "close";
+        span.appendChild(txt);
+        myNodelist[i].appendChild(span);
+    }
+
+    // Click on a close button to hide the current list item
+    var close = document.getElementsByClassName("close");
+    var i;
+    for (i = 0; i < close.length; i++) {
+        close[i].onclick = function() {
+            var div = this.parentElement;
+            div.style.display = "none";
+        }
+    }
+
+    // Create a new list item when clicking on the "Add" button
+    function newElement() {
+        var li = document.createElement("li");
+        var inputValue = document.getElementById("myInput").value;
+        var t = document.createElement("input");
+        t.value = inputValue;
+        t.setAttribute("class", "form-control");
+        t.setAttribute("name", "vector[]");
+        li.appendChild(t);
+        if (inputValue === '') {
+            alert("You must write something!");
+        } else {
+            document.getElementById("myUL").appendChild(li);
+        }
+        document.getElementById("myInput").value = "";
+
+        var span = document.createElement("SPAN");
+        var txt = document.createTextNode("\u00D7");
+        span.className = "close btn-default";
+        span.appendChild(txt);
+        li.appendChild(span);
+
+        for (i = 0; i < close.length; i++) {
+            close[i].onclick = function() {
+                var div = this.parentElement;
+                div.style.display = "none";
             }
         }
     }
