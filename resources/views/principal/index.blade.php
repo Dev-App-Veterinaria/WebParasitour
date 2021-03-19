ce690fc86ede7d11f72f3ec26b2113d54bb15d63
Victor Andrade <victor.hugooo@hotmail.com.br>
2021-03-19 20:13:30 -0300

    Ajustes no footer

--- resources/views/principal/index.blade.php
+++ resources/views/principal/index.blade.php
@@ -31,41 +31,4 @@
         </div>
     </div>
 
-    <!-- Créditos -->
-    <div class="page-footer-section bg-dark fg-white">
-        <div class="container">
-            <div class="row mb-5 py-3">
-                <div class="col-sm-6 py-3">
-                    <h5 class="mb-3">Sistema desenvolvido por alunos da<br>
-                    Universidade Federal do Agreste de Pernambuco<br>
-                    e do Instituto Federal de Pernambuco.</h5>
-                </div>
-                <div class="col-md-6 col-lg-4 py-3">
-                    <h5 class="mb-3">Grupos de pesquisa envolvidos:</h5>
-                    <p>• UNAME RESEARCH GROUP</p>
-                    <p>• LAPAR</p>
-                </div>
-            </div>
-        </div>
-        <hr>
-        <div class="container">
-            <div class="row">
-                <div class="col-12 col-md-6 py-2">
-                    <img src="/assets/favicon-light.png" alt="" width="40">
-                    <p class="d-inline-block ml-2">Créditos template: &copy; <a href="https://www.macodeid.com/" class="fg-white fw-medium">MACode ID</a>.</p>
-                </div>
-            </div>
-        </div>
-    </div>
-
-    <script src="/assets/js/jquery-3.5.1.min.js"></script>
-
-    <script src="/assets/js/bootstrap.bundle.min.js"></script>
-
-    <script src="/assets/vendor/owl-carousel/js/owl.carousel.min.js"></script>
-
-    <script src="/assets/vendor/wow/wow.min.js"></script>
-
-    <script src="/assets/js/mobster.js"></script>
-
 @endsection
--- resources/views/template/template.blade.php
+++ resources/views/template/template.blade.php
@@ -9,11 +9,13 @@
     <meta name="copyright" content="MACode ID, https://www.macodeid.com/">
     <title>Parasitour</title>
     <link rel="shortcut icon" href="../assets/favicon.png" type="image/x-icon">
+    <!-- Imports de estilo do template -->
     <link rel="stylesheet" href="/assets/css/maicons.css">
     <link rel="stylesheet" href="/assets/vendor/animate/animate.css">
     <link rel="stylesheet" href="/assets/vendor/owl-carousel/css/owl.carousel.min.css">
     <link rel="stylesheet" href="/assets/css/bootstrap.css">
     <link rel="stylesheet" href="/assets/css/mobster.css">
+    <!-- Imports dos scripts -->
     <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
         integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous">
@@ -28,6 +30,32 @@
 
 <body>
     @yield('conteudo')
+        <!-- Créditos -->
+        <div class="page-footer-section bg-dark fg-white">
+        <div class="container">
+            <div class="row mb-5 py-3">
+                <div class="col-sm-6 py-3">
+                    <h5 class="mb-3">Sistema desenvolvido por alunos da<br>
+                    Universidade Federal do Agreste de Pernambuco<br>
+                    e do Instituto Federal de Pernambuco.</h5>
+                </div>
+                <div class="col-md-6 col-lg-4 py-3">
+                    <h5 class="mb-3">Grupos de pesquisa envolvidos:</h5>
+                    <p>• UNAME RESEARCH GROUP</p>
+                    <p>• LAPAR</p>
+                </div>
+            </div>
+        </div>
+        <hr>
+        <div class="container">
+            <div class="row">
+                <div class="col-12 col-md-6 py-2">
+                    <img src="/assets/favicon-light.png" alt="" width="40">
+                    <p class="d-inline-block ml-2">Créditos template: &copy; <a href="https://www.macodeid.com/" class="fg-white fw-medium">MACode ID</a>.</p>
+                </div>
+            </div>
+        </div>
+    </div>
 </body>
 
 </html>
