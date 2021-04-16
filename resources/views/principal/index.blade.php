6cc3d4724bb34a065536495999ddc1dda9d85878
Victor Andrade <victor.hugooo@hotmail.com.br>
2021-04-15 21:07:17 -0300

    removido alerta da tela inicial

--- resources/views/principal/index.blade.php
+++ resources/views/principal/index.blade.php
@@ -1,11 +1,6 @@
 @extends('template.template')
 
 @section('conteudo')
-@if ($alert = Session::get('alert-success'))
-	<div class="alert">
-		{{ $alert }}
-	</div>
-@endif
 
 <div class="page-hero-section bg-image hero-home-1" style="background-image: url(/assets/img/bg_hero_1.svg);">
     <div class="hero-caption pt-5">
